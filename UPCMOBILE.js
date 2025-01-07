ddea9acf2b185abad59ec996248bd32ff1dfb6a7
HowieWood <98788152+utopeadia@users.noreply.github.com>
2025-01-07 05:51:16 +0000

    默认使用dialer-proxy

--- UPCMOBILE.js
+++ UPCMOBILE.js
@@ -11,25 +11,26 @@ function main(config, profileName) {
     updateProxyOptionByGroup(config, "name", /.*/, "ip-version", "ipv4-prefer");
 
     // 使用aes128SS
-    // updateProxyOption(config, "name", /自建L/, "port", 11369)
-    // updateProxyOption(config, "name", /自建L/, "cipher", "aes-128-gcm")
+    updateProxyOption(config, "name", /自建L/, "port", 11369)
+    updateProxyOption(config, "name", /自建L/, "cipher", "aes-128-gcm")
 
     // 删除2022-blake3-aes-256-gcm节点
-    // removeProxiesByProperty(config, "cipher", "2022-blake3-aes-256-gcm");
+    removeProxiesByProperty(config, "cipher", "2022-blake3-aes-256-gcm");
 
     // 关闭自建落地TCP快速打开
     updateProxyOption(config, "name", /自建L/, "tfo", false)
 
     // 设置dialer-proxy
-    // updateDialerProxyGroup(config, [
-    //     ["🛬 新加坡落地", "🇸🇬 新加坡节点", "🦁 新加坡自建落地"],
-    //     ["🛬 美国落地", "🇺🇲 美国节点", "💵 美国自建落地"],
-    //     ["🛬 日本落地", "🇯🇵 日本节点", "🎎 日本自建落地"],
-    //     ["🛬 香港落地", "🇭🇰 香港节点", "🌷 香港自建落地"],
-    //     ["🛬 湾湾落地", "🐉 湾湾节点", "🍍 湾湾自建落地"],
-    //     ["🛬 西北欧落地", "🇪🇺 西北欧节点", "🗼 西北欧自建落地"],
-    //     ["🛬 任选落地", "🛫 任选前置", "🚡 任选落地"]
-    // ]);
+    updateDialerProxyGroup(config, [
+        ["🛬 新加坡落地", "🇸🇬 新加坡节点", "🦁 新加坡自建落地"],
+        ["🛬 美国落地", "🇺🇲 美国节点", "💵 美国自建落地"],
+        ["🛬 日本落地", "🇯🇵 日本节点", "🎎 日本自建落地"],
+        ["🛬 香港落地", "🇭🇰 香港节点", "🌷 香港自建落地"],
+        ["🛬 湾湾落地", "🐉 湾湾节点", "🍍 湾湾自建落地"],
+        ["🛬 西北欧落地", "🇪🇺 西北欧节点", "🗼 西北欧自建落地"],
+        ["🛬 任选落地", "🛫 任选前置", "🚡 任选落地"]
+    ]);
+    updateGroupOption(config, "type", ["load-balance"], "strategy", "round-robin");
 
     // 修改节点dialer-proxy (正则匹配)
     updateProxyOption(config, "name", /JP穿透SS-/, "dialer-proxy", "🇯🇵 日本节点");
@@ -40,7 +41,6 @@ function main(config, profileName) {
 
     // 修改订阅组选项
     updateGroupOption(config, "type", ["load-balance", "fallback", "url-test"], "lazy", true);
-    // updateGroupOption(config, "type", ["load-balance"], "strategy", "round-robin");
 
     // 修改节点 UDP over TCP 选项
     updateProxyOption(config, "type", ["vmess", "vless", "trojan", "ss", "ssr", "tuic"], "udp-over-tcp", true);
