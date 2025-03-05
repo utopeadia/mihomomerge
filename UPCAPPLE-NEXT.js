bff8116df9a459b0ff09bafa13d152950c80ba60
HowieWood <98788152+utopeadia@users.noreply.github.com>
2025-03-05 01:30:33 +0000

    del default-nameserver Domain DH-DNS

--- UPCAPPLE-NEXT.js
+++ UPCAPPLE-NEXT.js
@@ -17,13 +17,11 @@ function main(config, profileName) {
     // 添加DH-DNS上海
     updateDNS(config, [
         ["proxy-server-nameserver", "https://dh-dns.global-idt.net/dns-query#DIRECT&h3=true&skip-cert-verify=true"],
-        ["default-nameserver", "https://dh-dns.global-idt.net/dns-query#DIRECT&h3=true&skip-cert-verify=true"],
         ["fallback", "https://dh-dns.global-idt.net/dns-query#DIRECT&h3=true&skip-cert-verify=true"]
     ]);
     // 添加DH-DNS北京
     updateDNS(config, [
         ["proxy-server-nameserver", "https://north.dh-global-team.net:438/dns-query#DIRECT&h3=true&skip-cert-verify=true"],
-        ["default-nameserver", "https://north.dh-global-team.net:438/dns-query#DIRECT&h3=true&skip-cert-verify=true"],
         ["fallback", "https://north.dh-global-team.net:438/dns-query#DIRECT&h3=true&skip-cert-verify=true"]
     ]);
 
