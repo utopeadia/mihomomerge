2b6d17ebe4e65964929dcfb9e29f007f2da951d8
HowieWood <98788152+utopeadia@users.noreply.github.com>
2025-03-16 12:56:58 +0800

    Update UPCAPPLE-NEXT.js

--- UPCAPPLE-NEXT.js
+++ UPCAPPLE-NEXT.js
@@ -48,6 +48,10 @@ function main(config, profileName) {
     // updateProxyOption(config, "name", /自建L/, "cipher", "aes-128-gcm")
     // // 删除2022-blake3-aes-128-gcm节点
     // removeProxiesByProperty(config, "cipher", "2022-blake3-aes-128-gcm");
+    // // 删除vless节点
+    // removeProxiesByProperty(config, "type", "vless");
+    // 删除hysteria2节点
+    removeProxiesByProperty(config, "type", "hysteria2");
 
     // 关闭自建落地TCP快速打开
     updateProxyOption(config, "name", /自建L/, "tfo", false);
